ea7f253f0d2ef21b234b5d12563b34c2287e16c6
Agent Y <yulive.270@gmail.com>
2025-10-06 22:36:32 +0100

    add a spigot logo as ascii in the logger messages!

--- src/main/java/net/yuflow/proxyflow/ProxyFlowSpigot.java
+++ src/main/java/net/yuflow/proxyflow/ProxyFlowSpigot.java
@@ -16,17 +16,21 @@ public class ProxyFlowSpigot extends JavaPlugin {
         this.getServer().getPluginManager().registerEvents(new ConnectionListener(this.getLogger(), this.configManager), this);
         this.getServer().getPluginManager().registerEvents(new MaintenanceListener(this.configManager), this);
 
-        getLogger().info(" ");
-        getLogger().info(" _______  ______    _______  __   __  __   __  _______  ___      _______  _     _    __   __  ____  ");
-        getLogger().info("|       ||    _ |  |       ||  |_|  ||  | |  ||       ||   |    |       || | _ | |  |  | |  ||    | ");
-        getLogger().info("|    _  ||   | ||  |   _   ||       ||  |_|  ||    ___||   |    |   _   || || || |  |  |_|  | |   | ");
-        getLogger().info("|   |_| ||   |_||_ |  | |  ||       ||       ||   |___ |   |    |  | |  ||       |  |       | |   | ");
-        getLogger().info("|    ___||    __  ||  |_|  | |     | |_     _||    ___||   |___ |  |_|  ||       |  |       | |   | ");
-        getLogger().info("|   |    |   |  | ||       ||   _   |  |   |  |   |    |       ||       ||   _   |   |     |  |   | ");
-        getLogger().info("|___|    |___|  |_||_______||__| |__|  |___|  |___|    |_______||_______||__| |__|    |___|   |___| ");
-        getLogger().info(" ");
-        getLogger().info("                      ProxyFlow v1.1.2-Spigot powered by Yu_Dino");
-        getLogger().info("                     Plugin has been initialized successfully!");
-        getLogger().info(" ");
+        getLogger().info("                                                                                                                    @###%@#**@%##**");
+        getLogger().info("                                                                                                                   %%###%%###%%%%%%");
+        getLogger().info(" _______  ______    _______  __   __  __   __  _______  ___      _______  _     _    __   __  ____                       %%#*");
+        getLogger().info("|       ||    _ |  |       ||  |_|  ||  | |  ||       ||   |    |       || | _ | |  |  | |  ||    |                      %%##");
+        getLogger().info("|    _  ||   | ||  |   _   ||       ||  |_|  ||    ___||   |    |   _   || || || |  |  |_|  | |   |                     %####*@");
+        getLogger().info("|   |_| ||   |_||_ |  | |  ||       ||       ||   |___ |   |    |  | |  ||       |  |       | |   |                    %@#####@@");
+        getLogger().info("|    ___||    __  ||  |_|  | |     | |_     _||    ___||   |___ |  |_|  ||       |  |       | |   |                   %@########");
+        getLogger().info("|   |    |   |  | ||       ||   _   |  |   |  |   |    |       ||       ||   _   |   |     |  |   |                   %@#######*@");
+        getLogger().info("|___|    |___|  |_||_______||__| |__|  |___|  |___|    |_______||_______||__| |__|    |___|   |___|   %+++++*@#%%%%*##%%%##%%%#%+##");
+        getLogger().info("                                                                                                     @%*++++%+%%#+*+*+%%+%+%@*+#+##%");
+        getLogger().info("                      ProxyFlow v1.1.2-Spigot powered by Yu_Dino                                     %******%+***#*#*++++#+***#**######@");
+        getLogger().info("                     Plugin has been initialized successfully!                                       @@%@%%%**%%%%%%#***#%%%%#%%#%%%%%%#%");
+        getLogger().info("                                                                                                       %%%%%%%#%%%%@@%%%%%%%%%%%%%@%%%%%###%");
+        getLogger().info("                                                                                                        %%%%%@@#%     %%%%%%%%%%%%%%%%%%%%%#@");
+        getLogger().info("                                                                                                                                %%%%%%%%%%%%");
+        getLogger().info("                                                                                                                                 %%%%%%%@%");
     }
 }
